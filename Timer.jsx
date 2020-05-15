bd0b316b8d5359b1fb6b06b0d64fcab18587e71c
Awab <awab.qureshi@gmail.com>
2020-05-15 17:15:20 +0500

    Minor commenting

--- Timer.jsx
+++ Timer.jsx
@@ -5,6 +5,7 @@ import Constants from 'expo-constants';
 const workTime = 20 * 60;
 const breakTime = 5 * 60;
 
+// Diplays the mode
 const Mode = (props) => {
     if (props.breakModeNext) {
         return ("Work!");
@@ -13,17 +14,27 @@ const Mode = (props) => {
     }
 }
 
+// converts the base 10 internal time to display time
+function timeConvert(num) {
+    let minutes = Math.floor(num / 60);
+    let seconds = num % 60;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+    return (minutes + ":" + seconds);
+}
+
 export default class Timer extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            utime: workTime,
-            timerPlay: true,
-            breakModeNext: true,
+            utime: workTime,        // time being counted down in base 10
+            timerPlay: true,        // is timer playing or not(paused)?
+            breakModeNext: true,    // is the next mode a break or not?
         }
     }
 
+    // Main render func of the app
     render() {
         return (
             <View>
@@ -50,6 +61,7 @@ export default class Timer extends React.Component {
         )
     }
 
+    // decreases the timer by 1
     timerTick = () => {
         this.setState( (prevState) => {
             return ({
@@ -58,9 +70,10 @@ export default class Timer extends React.Component {
                 breakModeNext: prevState.breakModeNext,
             });
         });
-        console.log("timer tick!");
+        // console.log("timer tick!");  // for debugging purposes
     }
 
+    // plays the timer. only works if timer not already playing
     playHandler = () => {
         if (this.state.timerPlay === false) {
             this.setState( (prevState) => {
@@ -70,10 +83,11 @@ export default class Timer extends React.Component {
                     breakModeNext: prevState.breakModeNext,
                 });
             });
-            this.intervalId = setInterval(this.timerTick,1000);
+            this.intervalId = setInterval(this.timerTick,1000);     // stores the id of setInterval that decreases timer by 1 every sec
         }
     }
 
+    // pauses the timer. only works if timer not already paused.
     pauseHandler = () => {
         if (this.state.timerPlay) {
             this.setState( (prevState) => {
@@ -83,10 +97,11 @@ export default class Timer extends React.Component {
                     breakModeNext: prevState.breakModeNext,
                 });
             });
-            clearInterval(this.intervalId);
+            clearInterval(this.intervalId);     // kills the interval set when timer played
         }
     }
 
+    // resets state to initial state
     resetHandler = () => {
         this.setState({
             utime: workTime,
@@ -95,14 +110,18 @@ export default class Timer extends React.Component {
         });
     }
 
+    // plays timer on start up
     componentDidMount() {
         this.intervalId = setInterval(this.timerTick,1000);
     }
 
+    // redundant failsafe to kill interval if component no longer rendering
     componentWillUnmount() {
         clearInterval(this.intervalId);
     }
 
+    // switches mode between break and work
+    // needs more commenting
     modeSwitcher() {
         let time = null;
         let nextModeIsBreak = null;
@@ -123,6 +142,7 @@ export default class Timer extends React.Component {
         ));
     }
 
+    // checks if timer is at 0 and then switches mode
     shouldComponentUpdate() {
         if (this.state.utime <= 0) {
             this.modeSwitcher()
@@ -131,16 +151,6 @@ export default class Timer extends React.Component {
         return true;
     }
 }
-
-
-function timeConvert(num) {
-    let minutes = Math.floor(num / 60);
-    let seconds = num % 60;
-    if (minutes < 10) minutes = "0" + minutes;
-    if (seconds < 10) seconds = "0" + seconds;
-    return (minutes + ":" + seconds);
-}
-
 
 const styles = StyleSheet.create({
     timerContainer: {
