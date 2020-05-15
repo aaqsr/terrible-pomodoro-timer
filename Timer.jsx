50ef32b3c0b442b25ecb0b2b98c434863c795bb1
Awab <awab.qureshi@gmail.com>
2020-05-15 16:21:30 +0500

    Added siwtching to break mode

--- Timer.jsx
+++ Timer.jsx
@@ -7,19 +7,29 @@ export default class Timer extends React.Component {
         super(props);
 
         this.state = {
-            utime: 68,
-            timerPlay: true
+            utime: 6,
+            // utime: 20 * 60,
+            timerPlay: true,
+            breakModeNext: true,
         }
     }
 
     render() {
         return (
             <View>
-            <Text style={styles.timerContainer}>
+            <View style={styles.timerContainer}>
+            <Text style={styles.timerTextContainer}>
             {timeConvert(this.state.utime)}
             </Text>
-            <Button onPress={this.playHandler} title="Play"/>
-            <Button onPress={this.pauseHandler} title="Pause"/>
+            </View>
+            <View style={styles.buttonContainer}>
+            <View style={styles.buttonSpacing}>
+            <Button onPress={this.playHandler} title="Play" />
+            </View>
+            <View style={styles.buttonSpacing}>
+            <Button onPress={this.pauseHandler} title="Pause" />
+            </View>
+            </View>
             </View>
         )
     }
@@ -29,32 +39,35 @@ export default class Timer extends React.Component {
             return ({
                 utime: prevState.utime - 1,
                 timerPlay: prevState.timerPlay,
+                breakModeNext: prevState.breakModeNext,
             });
         });
         console.log("timer tick!");
     }
 
-       playHandler = () => {
+    playHandler = () => {
         if (this.state.timerPlay === false) {
             this.setState( (prevState) => {
                 return ({
                     utime: prevState.utime,
                     timerPlay: true,
+                    breakModeNext: prevState.breakModeNext,
                 });
             });
             this.intervalId = setInterval(this.timerTick,1000);
         }
     }
 
- pauseHandler = () => {
+    pauseHandler = () => {
         if (this.state.timerPlay) {
-        this.setState( (prevState) => {
-            return ({
-                utime: prevState.utime,
-                timerPlay: false,
+            this.setState( (prevState) => {
+                return ({
+                    utime: prevState.utime,
+                    timerPlay: false,
+                    breakModeNext: prevState.breakModeNext,
+                });
             });
-        });
-        clearInterval(this.intervalId);
+            clearInterval(this.intervalId);
         }
     }
 
@@ -66,14 +79,36 @@ export default class Timer extends React.Component {
         clearInterval(this.intervalId);
     }
 
+    modeSwitcher() {
+        let time = null;
+        let nextModeIsBreak = null;
+        if (this.state.breakModeNext) {
+            time = 5
+            nextModeIsBreak = false;
+        } else {
+            time = 8
+            nextModeIsBreak = true;
+        }
+        // if (this.state.breakModeNext) { time = 5 * 60 } else { time = 20 * 60 }
+
+        this.setState( (prevState) => (
+            {
+                utime: time,
+                timerPlay: prevState.timerPlay,
+                breakModeNext: nextModeIsBreak,
+            }
+        ));
+    }
+
+
     shouldComponentUpdate() {
         if (this.state.utime <= 0) {
-            clearInterval(this.intervalId);
-            return false;
+            this.modeSwitcher()
         }
         return true;
     }
 }
+
 
 function timeConvert(num) {
     let minutes = Math.floor(num / 60);
@@ -86,8 +121,25 @@ function timeConvert(num) {
 
 const styles = StyleSheet.create({
     timerContainer: {
-        fontSize: 60,
-        paddingTop: Constants.statusBarHeight * 2.5,
-        alignSelf: "center",
+        borderColor: "blue",
+        borderStyle: "solid",
+        borderRadius: 5,
+        borderWidth: 5,
+        marginTop: Constants.statusBarHeight * 2.5,
     },
-    })
+    timerTextContainer: {
+        fontSize: 60,
+        alignSelf: "center",
+
+    },
+    buttonContainer: {
+        // flexDirection: "row",
+        width: "90%",
+        alignSelf: "center",
+        fontSize: 30,
+        margin: 10,
+    },
+    buttonSpacing: {
+        margin: 5,
+    },
+})
